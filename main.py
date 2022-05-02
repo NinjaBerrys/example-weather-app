3288e19a7c893e17c764602992eef6bc4978f5c9
brianmao <ninjaberrys5355@gmail.com>
2022-05-03 10:02:37 +1200

    part 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Search_image = PhotoImage(file="img/search.png")
 myimage = Label(image=Search_image)
 myimage.place(x=20, y=20)
 
-textfield = tk.Entry(root, justify="center", width=17, font=("poppins", 25, "bold"), bg="#404040",
+textfield = tk.Entry(root, justify="center", width=17, font=("comic sans ms", 25, "bold"), bg="#404040",
                      border=0, fg="white")
 
 textfield.place(x=50, y=40)
@@ -32,36 +32,36 @@ frame_myimage = Label(image=Frame_image)
 frame_myimage.pack(padx=5, pady=5, side=BOTTOM)
 
 # time
-name = Label(root, font=("arial", 15, "bold"))
+name = Label(root, font=("comic sans ms", 15, "bold"))
 name.place(x=30, y=100)
-clock = Label(root, font=("Helvetica", 20))
+clock = Label(root, font=("comic sans ms", 20))
 clock.place(x=30, y=130)
 
 # label
-label1 = Label(root, text="WIND", font=("Helvetica", 15, 'bold'), fg='white', bg='#1ab5ef')
+label1 = Label(root, text="WIND", font=("comic sans ms", 15, 'bold'), fg='white', bg='#1ab5ef')
 label1.place(x=120, y=400)
 
-label2 = Label(root, text="HUMIDITY", font=("Helvetica", 15, 'bold'), fg='white', bg='#1ab5ef')
+label2 = Label(root, text="HUMIDITY", font=("comic sans ms", 15, 'bold'), fg='white', bg='#1ab5ef')
 label2.place(x=225, y=400)
 
-label3 = Label(root, text="DESCRIPTION", font=("Helvetica", 15, 'bold'), fg='white', bg='#1ab5ef')
+label3 = Label(root, text="DESCRIPTION", font=("comic sans ms", 15, 'bold'), fg='white', bg='#1ab5ef')
 label3.place(x=430, y=400)
 
-label4 = Label(root, text="PRESSURE", font=("Helvetica", 15, 'bold'), fg='white', bg='#1ab5ef')
+label4 = Label(root, text="PRESSURE", font=("comic sans ms", 15, 'bold'), fg='white', bg='#1ab5ef')
 label4.place(x=620, y=400)
 
-t = Label(font=("arial", 70, "bold"), fg='#ee666d')
+t = Label(font=("comic sans ms", 70, "bold"), fg='#ee666d')
 t.place(x=400, y=150)
-c = Label(font=("arial", 15, 'bold'))
+c = Label(font=("comic sans ms", 15, 'bold'))
 c.place(x=400, y=250)
 
-w = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
+w = Label(text="...", font=("comic sans ms", 20, "bold"), bg="#1ab5ef")
 w.place(x=120, y=430)
-h = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
+h = Label(text="...", font=("comic sans ms", 20, "bold"), bg="#1ab5ef")
 h.place(x=280, y=430)
-d = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
+d = Label(text="...", font=("comic sans ms", 20, "bold"), bg="#1ab5ef")
 d.place(x=450, y=430)
-p = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
+p = Label(text="...", font=("comic sans ms", 20, "bold"), bg="#1ab5ef")
 p.place(x=670, y=430)
 
 root.mainloop()
