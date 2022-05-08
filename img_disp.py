6f702cfd25907a4c9c6b40576f9e5102731ad616
brianmao <ninjaberrys5355@gmail.com>
2022-05-09 11:52:17 +1200

    part 4

--- img_disp.py
+++ img_disp.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image
 from PIL import ImageTk
+from PIL import GifImagePlugin
 
 # image as a clickable button
 master = Tk()
@@ -12,7 +13,7 @@ def callback():
 
 width = 850
 height = 450
-img = Image.open("img/search.png")
+img = Image.open("img/egg.gif")
 img = img.resize((width, height))
 photoImg = ImageTk.PhotoImage(img)
 b = Button(master, image=photoImg, command=callback, width=850)
